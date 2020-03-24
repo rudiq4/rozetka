7d58ad4642abb08823f461d77d30e54f6161cfa0
rudiq4 <rudikvovan@gmail.com>
2020-03-24 18:58:20 +0200

    paeginator add

--- shop/views.py
+++ shop/views.py
@@ -1,19 +1,35 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Product, Category
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from cart.forms import CartAddProductForm
 
 
 def product_list(request, category_slug=None):
+    template = 'shop/product/list.html'
     category = None
     categories = Category.objects.all()
     products = Product.objects.filter(available=True)
+    page = request.GET.get('page')
     if category_slug:
         category = get_object_or_404(Category, slug=category_slug)
         products = products.filter(category=category)
-    return render(request, 'shop/product/list.html',
-                  {'category': category,
-                   'categories': categories,
-                   'products': products})
+        paginator = Paginator(products, 3)
+        try:
+            products = paginator.page(page)
+        except PageNotAnInteger:
+            products = paginator.page(1)
+        except EmptyPage:
+            products = paginator.page(paginator.num_pages)
+    else:
+        paginator = Paginator(products, 3)
+        try:
+            products = paginator.page(page)
+        except PageNotAnInteger:
+            products = paginator.page(1)
+        except EmptyPage:
+            products = paginator.page(paginator.num_pages)
+    return render(request, template,
+                  {'category': category, 'categories': categories, 'products': products, 'page': page})
 
 
 def product_detail(request, id, slug):
